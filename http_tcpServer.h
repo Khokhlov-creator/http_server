7144ff8464e6d968f37c6c118ba10c61f36a5579
Khokhlov-creator <manus71111@gmail.com>
2023-04-24 12:33:42 +0200

    made TcpServer constructor/destructor

---
+++ http_tcpServer.h
@@ -0,0 +1,31 @@
+#ifndef HTTP_SERVER_HTTP_TCPSERVER_H
+#define HTTP_SERVER_HTTP_TCPSERVER_H
+
+#include <stdio.h>
+#include <winsock.h>
+#include <stdlib.h>
+#include <string>
+
+namespace http{
+    class TcpServer{
+    public:
+        TcpServer(std::string ip_address, int port);
+        ~TcpServer();
+
+    private:
+        std::string m_ip_address;
+        int m_port;
+        SOCKET m_socket;
+        SOCKET m_new_socket;
+        long m_incomingMessage;
+        struct sockaddr_in m_socketAddress;
+        int m_socketAddress_len;
+        std::string m_serverMessage;
+        WSADATA m_wsaData;
+
+        int startServer();
+        coid closeServer();
+    };
+}//namespace http;
+
+#endif //HTTP_SERVER_HTTP_TCPSERVER_H
